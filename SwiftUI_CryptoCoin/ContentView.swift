43bdc9b0568133c2290e67189d26793ad21580b2
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-09-06 16:44:19 +0900

    [Setting] project 초기설정

---
+++ SwiftUI_CryptoCoin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_CryptoCoin
+//
+//  Created by Jisoo Ham on 9/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_CryptoCoin/SwiftUI_CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CryptoCoinApp.swift
+//  SwiftUI_CryptoCoin
+//
+//  Created by Jisoo Ham on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
