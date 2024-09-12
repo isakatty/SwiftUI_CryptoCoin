7affebbf9e0337204b3f49f94121a224de4dd2e7
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-09-12 20:24:12 +0900

    [Remove] Response 모델 삭제

--- SwiftUI_CryptoCoin/Network/SearchResponseDTO.swift
+++ SwiftUI_CryptoCoin/Network/SearchResponseDTO.swift
@@ -17,26 +17,3 @@ struct CoinDetailResponseDTO: Decodable, Identifiable {
     let thumb: String
     
 }
-extension SearchResponseDTO {
-    var toDomain: SearchResponse {
-        return .init(
-            coins: coins.map { CoinDetailResponse.init(
-                id: $0.id,
-                name: $0.name,
-                symbol: $0.symbol,
-                thumb: $0.thumb
-            )}
-        )
-    }
-}
-
-struct SearchResponse {
-    var coins: [CoinDetailResponse]
-}
-struct CoinDetailResponse: Identifiable {
-    let id: String
-    let name: String
-    let symbol: String
-    let thumb: String
-    var isFav: Bool = false
-}
